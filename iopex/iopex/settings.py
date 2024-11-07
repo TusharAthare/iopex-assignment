b326c57db9526bf1a4ded0f948b6505ee2826e2f
tushar-quartic <tushar@quartic.ai>
2024-11-07 13:53:39 +0530

    Add delete method for tasks

--- iopex/iopex/settings.py
+++ iopex/iopex/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
--- iopex/tasks/views.py
+++ iopex/tasks/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from tasks.serializers import TaskSerializer
 from tasks.models import Tasks
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ from tasks.models import Tasks
 class TasksView(APIView):
     """Creates user for specified email and name
     """
+    
     def post(self, request):
         """Creation of tasks
         """
@@ -18,8 +21,7 @@ class TasksView(APIView):
             serializer = TaskSerializer(data=request.data)
             return self.serializer_validations(serializer)
         except Exception as e:
-            print(f"{e=}")
-            return Response(data= {}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
     def get(self, request):
         """Fetches single task
@@ -30,7 +32,7 @@ class TasksView(APIView):
             serializer.is_valid(raise_exception=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(data= {}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
     
     def put(self, request):
@@ -38,7 +40,7 @@ class TasksView(APIView):
             serializer = TaskSerializer(data=request.data)
             return self.serializer_validations(serializer)
         except Exception as e:
-            return Response(data= {}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
     
     def patch(self, request):
@@ -46,7 +48,7 @@ class TasksView(APIView):
             serializer = TaskSerializer(data=request.data, partial=True)
             return self.serializer_validations(serializer)
         except Exception as e:
-            return Response(data= {}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
         
     def list(self, request):
@@ -58,7 +60,18 @@ class TasksView(APIView):
             serializer.is_valid(raise_exception=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(data= {}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    def delete(self, request):
+        try:
+            task = Tasks.objects.get(**request.data)
+            task.delete()
+            return Response(data = {"message": "Task Was deleted"}, status=status.HTTP_200_OK)
+        except (Tasks.DoesNotExist, Tasks.AttributeError) as e:
+            return Response(data = {"message": f"An Error occured -- {e}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+    
     def serializer_validations(self, serializer):
         """Gathered repeated code in one place
         """
